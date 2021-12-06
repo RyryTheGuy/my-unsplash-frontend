54bdf7de3891bdd4ff3caba9b060b0057b229b27
RyryTheGuy <ryanroemer61@gmail.com>
2021-12-06 10:06:01 -0600

    Added Masonry to the image grid

--- src/Components/ImageGrid/ImageCard/ImageCard.tsx
+++ src/Components/ImageGrid/ImageCard/ImageCard.tsx
@@ -4,21 +4,36 @@ import LazyLoad from 'react-lazyload';
 
 interface Props {
   image: any;
+  title: string;
 }
 
-const ImageCard = ( { image }: Props ) => {
+interface LazyLoadWithRef extends LazyLoad {
+  ref: HTMLDivElement;
+}
+
+const ImageCard = ( { image, title }: Props ) => {
   const [ showOverlay, setShowOverlay ] = React.useState( false );
   const img = React.useRef<HTMLImageElement>( null );
+  const card = React.useRef<LazyLoadWithRef>( null );
+
+  const deleteCard = () => {
+    if ( card.current ) {
+      // FIXME: Add a way to remove the image from the database/storage
+      card.current.ref.remove();
+    } else {
+      throw new Error( 'Unable to delete the image at this time. Try again later' );
+    }
+  };
 
   return (
-    <LazyLoad height={400} once>
+    // https://www.npmjs.com/package/react-lazyload
+    <LazyLoad ref={card} height={img.current?.naturalHeight} style={{ width: `31%`, margin: '0 .75rem 1rem 0' }} once>
       <div
         onMouseEnter={() => setShowOverlay( true )}
         onMouseLeave={() => setShowOverlay( false )}
         className="relative rounded-xl"
-        style={{ width: img.current?.naturalWidth + "px" }}
       >
-        <img src={image} ref={img} className='w-auto rounded-xl' />
+        <img src={image} ref={img} className='w-full rounded-xl' />
         <div
           id="overlay"
           style={{ display: showOverlay ? "inline-block" : "none" }}
@@ -26,11 +41,11 @@ const ImageCard = ( { image }: Props ) => {
         >
           <div className="flex flex-col justify-between w-full h-full">
             <div id="image-delete" className="text-right p-4">
-              <button className="border border-red-500 text-red-500 rounded-2xl px-4 py-1">Delete</button>
+              <button className="border border-red-500 text-red-500 rounded-2xl px-4 py-1" onClick={deleteCard}>Delete</button>
             </div>
 
-            <div id="image-title" className="px-4 py-2">
-              This is the Image title which will be dynamic!!
+            <div id="image-title" className="px-4 py-2 text-3xl">
+              {title}
             </div>
           </div>
         </div>
--- src/Components/ImageGrid/ImageGrid.tsx
+++ src/Components/ImageGrid/ImageGrid.tsx
@@ -1,23 +1,29 @@
 import React from "react";
+import Masonry from "react-masonry-component";
 import { ImageCard } from "./ImageCard/ImageCard";
+
 import carPic from '../../assets/firefox_2021-10-19_22-10-51.png';
 import colePic from '../../assets/Discord_2021-10-26_22-39-44.png';
 import iyanPic from '../../assets/10563067_665363856893211_469939799997423436_n2.jpg';
 
 
 const ImageGrid = () => {
-  // Implement the grid layout | https://github.com/react-grid-layout/react-grid-layout w/ https://www.npmjs.com/package/react-lazyload
+  // make the layout responsive
+  const options = {
+    columnWidth: 10,
+    horizontalOrder: true,
+    percentPosition: true,
+  };
 
   return (
-    <div>
-      Image Grid!
-      <ImageCard image={carPic} />
-      <ImageCard image={colePic} />
-      <ImageCard image={iyanPic} />
-      <ImageCard image={colePic} />
-      <ImageCard image={carPic} />
-      <ImageCard image={'https://firebasestorage.googleapis.com/v0/b/image-uploader-bf8fa.appspot.com/o/352c1ea9-d3c1-4256-bd1b-4e574e733252?alt=media&token=8efd6888-fd6d-4c68-a097-d51ec43a237b'} />
-    </div>
+    <Masonry className="w-full" options={options}>
+      <ImageCard image={carPic} title="Cool Car Picture" />
+      <ImageCard image={colePic} title="Some Soy Boy" />
+      <ImageCard image={iyanPic} title="Iyan, or someone" />
+      <ImageCard image={colePic} title="Soy again" />
+      <ImageCard image={carPic} title="Another car" />
+      <ImageCard image={'https://firebasestorage.googleapis.com/v0/b/image-uploader-bf8fa.appspot.com/o/352c1ea9-d3c1-4256-bd1b-4e574e733252?alt=media&token=8efd6888-fd6d-4c68-a097-d51ec43a237b'} title="Something" />
+    </Masonry>
   );
 };
 
